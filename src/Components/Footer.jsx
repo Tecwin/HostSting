1544045d9ef3e73400db3f7727fe21bd400285f0
Tecwin <tecwinrod@gmail.com>
2024-06-07 17:52:14 +0100

    fixed the footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className=' flex justify-between  mt-auto'>
+    <div className=' flex justify-between gap-4 mt-auto'>
       <ul className=' flex  gap-6 font-lato text-gray-400'>
         <li>
           <a href="#">Facebook</a>
@@ -14,8 +14,8 @@ const Footer = () => {
           <a href="#">Twitter</a>
         </li>
       </ul>
-    <div className='flex justify-between'> 
-      <img className=' w-16' src="./Assets/Expert.jpeg" alt="specialist"/>
+    <div className='flex justify-between gap-2' > 
+      <img className=' w-16 rounded-2xl' src="./Assets/Expert.jpeg" alt="specialist"/>
       <div>
         <p className=' font-playfair'>Have any Questions ?</p>
         <a  className=" font-lato"href="#" >Talk to Specialist</a>
